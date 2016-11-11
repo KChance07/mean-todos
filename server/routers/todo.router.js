7cae858866a0f92efb8cdbd463aaf30516419a3f
Kyle Chance <kylechance@gmail.com>
2016-11-10 19:50:52 -0500

    fixed ap crash because route function did not terminate on error

--- server/routers/todo.router.js
+++ server/routers/todo.router.js
@@ -9,22 +9,23 @@ router.use(bodyParser.urlencoded({extended: true}));
 router.get('/todos', function(req, res){
   Todo.find({}, function(err, foundTodos){
     if(err){
-      res.status(500).json({
+      return res.status(500).json({//this stops the function from running further and crashing your app ie if there is an error.
         err: err
       });
     }
-    res.status(200).json({
+    return res.status(200).json({
       todos: foundTodos
     });
   });
 });
 
-router.get('/todos/:id', function(req, res){
+router.get('/todos/:id', function(req, res, next){
   Todo.find({_id: req.params.id}, function(err, foundTodo){
     if(err){
       res.status(500).json({
         err: err
       });
+      next(); // this will stop the application from running futher in the function and crashing the app
     }
     res.status(200).json({
       todo: foundTodo
@@ -39,16 +40,18 @@ router.post('/todos', function(req, res){
       res.status(500).json({
         err: err
       });
+    } else { //this will keep the app from trying to send two responses and crashing the application
+      res.status(201).json({
+        msg: 'successfully created todo'
+      });
     }
-    res.status(201).json({
-      msg: 'successfully created todo'
-    });
+
   });
 });
 
 router.put('/todos', function(req, res){
   Todo.findOneAndUpdate({_id: req.params.id}, req.body, function(err, oldTodo){
-    if(err){
+    if(err){ //if left like the following, teh function will try to return and crash it
       res.status(500).json({
         err: err
       });
@@ -61,7 +64,7 @@ router.put('/todos', function(req, res){
 
 router.delete('/todos', function(req, res){
   Todo.findOneAndRemove({ _id: req.params.id }, function (err, deletedTodo){
-    if(err){
+    if(err){ //if left like the following, teh function will try to return and crash it
       err: err
     }
     res.status(200).json({
@@ -71,7 +74,7 @@ router.delete('/todos', function(req, res){
 });
 router.get('/todos/description/:desc', function(req, res){
   Todo.find({ description: req.params.desc }, function(err, foundTodos){
-    if(err){
+    if(err){ //if left like the following, teh function will try to return and crash it
       res.status(500).json({
         err: err
       });
