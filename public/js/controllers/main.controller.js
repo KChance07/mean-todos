b6853075362a73f7f3008bfb866e4d9bdbd8cd35
Kyle Chance <kylechance@gmail.com>
2016-11-10 20:32:57 -0500

    established all get all route for db and backend

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -1,31 +1,30 @@
-
 (function() {
-  angular.module('mean-todos') //getter syntax
-  .controller('MainController', MainController);
+    angular.module('mean-todos'). //getter syntax
+    controller('MainController', MainController);
 
-  MainController.$inject = ['$scope', 'TodoService']; //what tools the MainController function needs
+    MainController.$inject = ['$scope', 'TodoService']; //what tools the MainController function needs
+    $scope.$watch(function(){})
+    function MainController($scope, TodoService) { //$scope is our bridge to the dom
+        $scope.todos = TodoService.get();
+        $scope.createTodo = createTodo;
+        $scope.deleteTodo = deleteTodo;
+        $scope.editTodo = editTodo;
+        $scope.saveTodo = saveTodo;
 
-  function MainController($scope, TodoService){ //$scope is our bridge to the dom
-    $scope.todos = TodoService.get();
-    $scope.createTodo = createTodo;
-    $scope.deleteTodo = deleteTodo;
-    $scope.editTodo = editTodo;
-    $scope.saveTodo = saveTodo;
+        function createTodo(newTodo) {
+            TodoService.create(newTodo);
+            $scope.newTodo = '';
+        }
+        function deleteTodo(index) {
+            TodoService.delete(index);
+        }
+        function editTodo(todo) {
+            todo.isBeingEdited = true;
+        }
+        function saveTodo(index, todo) {
+            TodoService.update(index, todo); //remove the .desc because data stuff is in the service
+            todo.isBeingEdited = false;
+        }
 
-    function createTodo(newTodo){
-      TodoService.create(newTodo);
-      $scope.newTodo = '';
     }
-    function deleteTodo(index){
-      TodoService.delete(index);
-    }
-    function editTodo(todo){
-      todo.isBeingEdited = true;
-    }
-    function saveTodo(index, todo){
-      TodoService.update(index, todo.desc);
-      todo.isBeingEdited = false;
-    }
-
-  }
 }());
--- public/js/services/todo.service.js
+++ public/js/services/todo.service.js
@@ -3,11 +3,31 @@
   angular.module('mean-todos')
         .factory('TodoService', TodoService);
 
-  TodoService.$inject = [];
+  TodoService.$inject = ['$http'];
 
-  function TodoService(){
+  function TodoService($http){
+    init();
+    var todo = [];
     return {
-
+      get: getAllTodos,
+      create: createOneTodo,
+      update: updateOneTodo,
+      delete: deleteOneTodo
     };
+    function init(){ //this is going to make our first data request upon file load
+      $http.get('/todos')
+            .then(function(response){
+              todos = response.data;
+            })
+            .catch(function(err){
+              console.log(err);
+            });
+    }
+    function getAllTodos(){
+      return todos;
+    }
+    function createOneTodo(todo){}
+    function updateOne(index, todo){}
+    function deleteOneTodo(index){}
   }
 }());
